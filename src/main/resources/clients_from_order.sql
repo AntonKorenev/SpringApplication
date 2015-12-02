23e9495fa79524e9f8a41362cf57cd259b2d452b
AntonKorenev <anton.korenev.3000@gmail.com>
2015-12-02 13:35:00 +0200

    New database structure and relational changes in sql files.

--- src/main/resources/clients_from_order.sql
+++ src/main/resources/clients_from_order.sql
@@ -2,5 +2,5 @@ CREATE VIEW ClientsFromOrder AS
 SELECT clients.id, clients.first_name, clients.last_name
 FROM clients
 INNER JOIN orders
-ON orders.order_number=clients.id
-ORDER BY order_number;
+ON orders.client_id=clients.id
+ORDER BY orders.client_id;
--- src/main/resources/products_from_order.sql
+++ src/main/resources/products_from_order.sql
@@ -2,5 +2,5 @@ CREATE VIEW ProductsFromOrder AS
 SELECT products.id, products.name, products.price
 FROM products
 INNER JOIN orders
-ON orders.order_number=products.id
-ORDER BY order_number;
+ON orders.id=products.orders_id
+ORDER BY orders.id;
