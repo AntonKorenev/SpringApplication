407254bf38d39303025c71c5841b44a90cac8678
AntonKorenev <anton.korenev.3000@gmail.com>
2015-12-04 16:47:10 +0200

    Mockito tests for ServletCSV.

--- src/test/java/com/company/spring_application/servlet/ServletCSVTest.java
+++ src/test/java/com/company/spring_application/servlet/ServletCSVTest.java
@@ -1,35 +1,55 @@
 package com.company.spring_application.servlet;
 
-import com.mockrunner.mock.web.WebMockObjectFactory;
-import com.mockrunner.servlet.ServletTestModule;
+
+import com.company.spring_application.mapper.MapperFromCSV;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
 
-import static junit.framework.TestCase.assertEquals;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
-public class ServletCSVTest {
-    private ServletTestModule tester;
-    private WebMockObjectFactory factory;
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration("classpath:web.xml")
+@ContextConfiguration("classpath:spring_config.xml")
+public class ServletCSVTest extends Mockito {
+    @Autowired
+    WebApplicationContext context;
+    ServletCSV servletCSV;
 
     @Before
     public void init() {
-        factory = new WebMockObjectFactory();
-        tester = new ServletTestModule(factory);
+        servletCSV = new ServletCSV();
     }
 
     @Test
     public void postReturnNotEmpty() throws Exception {
-        tester.addRequestParameter("fname", "Anton");
-        tester.addRequestParameter("lname", "Korenev");
-        tester.addRequestParameter("task", "buy");
-        tester.addRequestParameter("orders", "1 1000 tv,2 200 monitor");
+        HttpServletRequest httpRequest = mock(HttpServletRequest.class);
+        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
 
-        // instantiate the servlet
-        tester.createServlet(ServletCSV.class);
+        String s= "1,Anton,Korenev,buy\n1,1000,tv\n2,200,monitor";
+        // convert String into InputStream
+        InputStream is = new ByteArrayInputStream(s.getBytes());
 
-        tester.doPost();
+        // read it with BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-        assertEquals(200, factory.getMockResponse().getStatusCode());
+        File tmp = File.createTempFile("temp-file-name", ".tmp");
+        PrintWriter pr = new PrintWriter(tmp);
+
+        when(httpRequest.getReader()).thenReturn(br);
+        when(httpResponse.getWriter()).thenReturn(pr);
+
+        servletCSV.mapperFromCSV = (MapperFromCSV) context.getBean("mapperFromCSV");
+        servletCSV.doPost(httpRequest,httpResponse);
+
+        verify(httpRequest,atLeast(1)).getReader();
     }
 }
