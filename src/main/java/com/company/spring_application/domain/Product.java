e405eb45207be9c9b113be3c9876194e7fb7ed36
Anton Korenev <anton.korenev@sigma.software>
2015-12-28 15:55:09 +0200

    Hibernate tests works

--- src/main/java/com/company/spring_application/domain/Product.java
+++ src/main/java/com/company/spring_application/domain/Product.java
@@ -17,7 +17,7 @@ public class Product implements DOInterface {
     @Column(name = "price")
     private final double price;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = true)
     Order order;
 
